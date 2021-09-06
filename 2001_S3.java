f664ae43e9d854460ddcb849976e21f2a6bb236d
Claire Chen <81056267+claireluchen@users.noreply.github.com>
2021-09-06 19:14:40 -0400

    Update 2001_S3.java

--- 2001_S3.java
+++ 2001_S3.java
@@ -65,15 +65,15 @@ public class Main {
 	static StringTokenizer st;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static String next () throws IOException {
-	  while (st == null || !st.hasMoreTokens())
-	  st = new StringTokenizer(br.readLine().trim());
-	  return st.nextToken();
+		while (st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
 	}
 	static int readInt () throws IOException {
-	  return Integer.parseInt(next());
+		return Integer.parseInt(next());
 	}
 	static String readLine () throws IOException {
-	  return br.readLine().trim();
+		return br.readLine().trim();
 	}
 
 }
