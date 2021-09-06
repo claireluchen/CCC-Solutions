c47c643f99adfdf1bfc22290604501f909631449
Claire Chen <81056267+claireluchen@users.noreply.github.com>
2021-09-06 19:09:39 -0400

    Update 2001_S3.java

--- 2001_S3.java
+++ 2001_S3.java
@@ -3,17 +3,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static StringTokenizer st;
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-	static String next () throws IOException {
-	 	while (st == null || !st.hasMoreTokens())
-	 		st = new StringTokenizer(br.readLine().trim());
-	    return st.nextToken();
-	}
-	static int readInt () throws IOException {
-	    return Integer.parseInt(next());
-	}
 	
 	public static void main(String[] args) throws IOException{
 		ArrayList<String> roads = new ArrayList<>();
@@ -70,6 +59,21 @@ public class Main {
 			System.out.printf("There are %d disconnecting roads.%n", set.size());
 		}
 
+	}
+	
+	//fast i/o
+	static StringTokenizer st;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static String next () throws IOException {
+	  while (st == null || !st.hasMoreTokens())
+	  st = new StringTokenizer(br.readLine().trim());
+	  return st.nextToken();
+	}
+	static int readInt () throws IOException {
+	  return Integer.parseInt(next());
+	}
+	static String readLine () throws IOException {
+	  return br.readLine().trim();
 	}
 
 }
